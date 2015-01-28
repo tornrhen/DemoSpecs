1eda90f0456683915978ccc2dbae14c64f2c2571
Jesus Mendoza <allstarmagic@hotmail.com>
2015-01-27 20:41:04 -0600

    First commit

---
+++ SpecDemo.Specs/Demo.feature
@@ -0,0 +1,15 @@
+﻿Feature: Demo
+	As a math idiot
+	In order to avoid silly mistakes
+	I want to be told the sum of two numbers
+
+
+Scenario: Add two numbers
+	Given I have entered '50,70' into the calculator
+	When I press add
+	Then the result should be '120' on the screen
+
+Scenario: Add zero numbers
+	Given I have entered '' into the calculator
+	When I press add
+	Then the result should be '0' on the screen 
---
+++ SpecDemo.Specs/DemoSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecDemo.Specs
+{
+    [Binding]
+    public class DemoSteps
+    {
+        private Calculator calculator = new Calculator();
+        private int result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            calculator.AddNumber(p0);
+        }
+
+        [When(@"I press add")]
+public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+public void ThenTheResultShouldBeOnTheScreen(int p0)
+{
+   Assert.AreEqual(p0,result);
+}
+    }
+
+    internal class Calculator
+    {
+        List<int> numbers = new List<int>(); 
+        public void AddNumber(int number)
+        {
+            numbers.Add(number);
+        }
+
+        public int Add()
+        {
+            return numbers.Sum();
+        }
+    }
+}
